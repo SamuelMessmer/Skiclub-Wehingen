9e32cfb3e6f735d25830c14c1b1f6ff2b6032324
Samuel Messmer <samuel.messmer@web.de>
2024-12-30 17:56:00 +0100

    fix: spelling mistake

--- components/NewsletterCards.tsx
+++ components/NewsletterCards.tsx
@@ -6,7 +6,7 @@ import { Button } from "./ui/button";
 import React, { useEffect, useState } from "react";
 
 const NewsletterCards: React.FC = () => {
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [blogs, setBlogs] = useState<Blog[]>([]);
   const [windowWidth, setWindowWidth] = useState<number>(0); // Set initial width to 0
 
@@ -16,9 +16,9 @@ const NewsletterCards: React.FC = () => {
       const data = await response.json();
       setBlogs(data);
 
-      if (!data) {
-        setLoading(true);
-      }
+      if (!data)
+        setLoading(false);
+
     } catch (error) {
       console.error("Error fetching blogs:", error);
     }
@@ -42,19 +42,21 @@ const NewsletterCards: React.FC = () => {
 
   return (
     <div>
-      {loading ? <div className="flex gap-8 items-center justify-center text-white m-8">
-        <p>Blogeinträge konnten nicht geladen werden...</p>
-        <Button
-          onClick={() => {
-            location.reload()
-          }}
-          className="bg-white  hover:scale-105 duration-300 hover:bg-slate-50 shadow-md">
-          <span className="font-bold text-black shadow-sm">
-            Erneut versuchen!
-          </span>
-        </Button>
-      </div>
-        : (
+      {loading ?
+        (
+          <div className="flex gap-8 items-center justify-center text-white m-8">
+            <p>Blogeinträge konnten nicht geladen werden...</p>
+            <Button
+              onClick={() => {
+                location.reload()
+              }}
+              className="bg-white  hover:scale-105 duration-300 hover:bg-slate-50 shadow-md">
+              <span className="font-bold text-black shadow-sm">
+                Erneut versuchen!
+              </span>
+            </Button>
+          </div>
+        ) : (
           <div className="grid justify-center items-center grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 lg:gap-14 mb-4">
             {displayedBlogs().map((blog) => (
               <div key={blog.id} className="flex flex-col items-center gap-3 w-fit">
