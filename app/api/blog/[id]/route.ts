c3690f70283b57a277d5516766db308eb58dc60d
Samuel Messmer <samuel.messmer@web.de>
2025-02-03 12:46:02 +0100

    fix: request in GET Method

--- app/api/blog/[id]/route.ts
+++ app/api/blog/[id]/route.ts
@@ -2,7 +2,9 @@ import prisma from "@/prisma/lib/clientLinkData";
 import { NextRequest, NextResponse } from "next/server";
 import schema from "@/app/api/blog/schema";
 
-export async function GET({ params }: { params: { id: string } }) {
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { id: string } }) {
   const uniqueBlog = await prisma.blog.findUnique({
     where: {
       id: parseInt(params.id),
