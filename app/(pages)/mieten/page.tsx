770cccc699a13903fa4241e55e3e2252ab1b2876
Samuel Messmer <samuel.messmer@web.de>
2025-04-29 21:42:58 +0200

    update: besser UX für das Miet Formular (RHK + Zod auf Server seite)

--- app/(pages)/mieten/page.tsx
+++ app/(pages)/mieten/page.tsx
@@ -1,190 +1,205 @@
 "use client";
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import Link from "next/link";
 
+type FormValues = {
+    firstName: string;
+    lastName: string;
+    email: string;
+    message: string;
+}
+
 const FormularKontakt = () => {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-  const [success, setSuccess] = useState(false);
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, isSubmitting },
+        reset,
+    } = useForm<FormValues>();
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault()
-    setLoading(true);
+    const [success, setSuccess] = useState(false);
+    const [error, setError] = useState(false);
 
-    const emailData = {
-      firstName,
-      lastName,
-      email,
-      message,
+    const onSubmit = async (data: FormValues) => {
+        const response = await fetch("api/emails", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(data)
+        });
+
+        if (!response.ok) {
+            setError(true)
+            return;
+        }
+
+        setSuccess(true)
+        reset();
     }
 
-    const response = await fetch("api/emails", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(emailData)
-    });
+    return (
+        <div>
+            {/* HEADER */}
+            <section>
+                <div className="flex flex-col items-center mt-20 mb-12 sm:mb-32">
+                    <h1 className="self-start sm:self-center ml-2 font-extrabold text-[52px] sm:text-8xl sm1 xl:text-9xl tracking-tighter mb-0 sm:mb-4 z-10">
+                        Skihütte mieten
+                    </h1>
+                    <p className="text-orange-500 text-lg sm:text-2xl mx-4 md:self-center md:mx-12 sm:text-center">
+                        Kontaktieren Sie unseren Hüttenwart und vereinbaren Sie mit ihm
+                        einen geiegneten Termin zur Hüttenmietung!
+                    </p>
+                </div>
+            </section>
 
-    setLoading(false)
+            {/* KONTAKT-FORMULAR */}
+            <section>
+                <div className="bg-white mt-10">
+                    <div className="flex flex-col sticky top-24 md:mx-40">
+                        <Card className="mb-5 md:mb-24 mx-4 md:mx-14 flex flex-col items-center md:items-center animate-zoom zoom shadow-xl bg-[#EFF7FF]">
+                            <div className="">
+                                <CardHeader className=" font-bold text-3xl lg:text-5xl -ml-2 mb-4 mt-12">
+                                    Hier ihre Informationen angeben:
+                                </CardHeader>
+                                <CardContent className="text-lg sm:mb-4">
+                                    <form
+                                        onSubmit={handleSubmit(onSubmit)}
+                                        method="POST"
+                                        action={""} //safari auto fill umgehen
+                                        className="mb-10"
+                                    >
+                                        <div className="flex w-full gap-5 sm:gap-10 mb-4">
+                                            <div>
+                                                <label className="block font-semibold text-lg mb-2 ">
+                                                    Vorname:
+                                                </label>
+                                                <input
+                                                    {...register("firstName", {
+                                                        required: "Bitte geben Sie einen Vornamen an",
+                                                        minLength: {
+                                                            value: 2,
+                                                            message: "Bitte geben Sie einen gültigen Vornamen an"
+                                                        }
+                                                    })}
+                                                    type="text"
+                                                    placeholder="Ihren Name"
+                                                    className={`p-2 border border-orange-200 rounded-xl w-full ${errors.firstName && "border-2 border-red-500"}`}
+                                                />
+                                                {errors.firstName && (
+                                                    <p className="font-light text-xs text-red-500">{`${errors.firstName?.message}`}</p>
+                                                )}
+                                            </div>
+                                            <div>
+                                                <label className="block font-semibold text-lg mb-2 ">
+                                                    Nachname:
+                                                </label>
+                                                <input
+                                                    {...register("lastName", {
+                                                        required: "Bitte geben Sie einen Nachnamen an",
+                                                        minLength: {
+                                                            value: 2,
+                                                            message: "Bitte geben Sie einen gültigen Nachnamen an",
+                                                        }
+                                                    }
+                                                    )}
+                                                    type="text"
+                                                    placeholder="Ihren Name"
+                                                    className={`p-2 border border-orange-200 rounded-xl w-full ${errors.lastName && "border-2 border-red-500"}`}
+                                                />
+                                                {errors.lastName && (
+                                                    <p className="font-light text-xs text-red-500">{`${errors.lastName?.message}`}</p>
+                                                )}
+                                            </div>
+                                        </div>
 
-    if (!response.ok) {
-      setError(true)
-      return;
-    }
+                                        <div className="mb-4">
+                                            <label className="block font-semibold text-lg mb-2 ">
+                                                Ihre E-Mail:
+                                            </label>
+                                            <input
+                                                {...register("email", {
+                                                    required: "Bitte geben Sie eine E-Mail an",
+                                                    pattern: {
+                                                        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                                        message: "Bitte geben Sie eine gültige E-Mail an"
+                                                    }
+                                                })}
+                                                type="email"
+                                                placeholder="Ihre E-Mail"
+                                                className={`p-2 border border-orange-200 rounded-xl w-full ${errors.email && "border-2 border-red-500"}`}
+                                            />
+                                            {errors.email && (
+                                                <p className="font-light text-xs text-red-500">{`${errors.email?.message}`}</p>
+                                            )}
+                                        </div>
 
-    setSuccess(true)
-  }
+                                        <div className="mb-4">
+                                            <label className="block font-semibold text-lg mb-2">
+                                                Ihre Nachricht:
+                                            </label>
+                                            <textarea
+                                                {...register("message", {
+                                                    minLength: {
+                                                        value: 5,
+                                                        message: "Bitte geben Sie eine gültige Nachricht ein (min. 5 Zeichen)"
+                                                    }
+                                                })}
+                                                placeholder="Ihre Nachricht - Wann Sie die Skihütte mieten möchten, wie Sie diese verwenden möchten"
+                                                className={`p-2 border border-orange-200 rounded-xl w-full h-56 ${errors.message && "border-2 border-red-500"}`}
+                                            />
+                                            {errors.message && (
+                                                <p className="font-light text-xs text-red-500">{`${errors.message?.message}`}</p>
+                                            )}
+                                        </div>
 
-  return (
-    <div>
-      <section>
-        <div className="flex flex-col items-center mt-20 mb-12 sm:mb-32">
-          <h1 className="self-start sm:self-center ml-2 font-extrabold text-[52px] sm:text-8xl sm1 xl:text-9xl tracking-tighter mb-0 sm:mb-4 z-10">
-            Skihütte mieten
-          </h1>
-          <p className="text-orange-500 text-lg sm:text-2xl mx-4 md:self-center md:mx-12 sm:text-center">
-            Kontaktieren Sie unseren Hüttenwart und vereinbaren Sie mit ihm
-            einen geiegneten Termin zur Hüttenmietung!
-          </p>
-        </div>
-      </section>
-
-      {/* KONTAKT-FORMULAR */}
-      <section>
-        <div className="bg-white mt-10">
-          <div className="flex flex-col sticky top-24 md:mx-40">
-            <Card className="mb-5 md:mb-24 mx-4 md:mx-14 flex flex-col items-center md:items-center animate-zoom zoom shadow-xl bg-[#EFF7FF]">
-              <div className="">
-                <CardHeader className=" font-bold text-3xl lg:text-5xl -ml-2 mb-4 mt-12">
-                  Hier ihre Informationen angeben:
-                </CardHeader>
-                <CardContent className="text-lg sm:mb-4">
-                  <form
-                    // action="https://api.web3forms.com/submit"
-                    onSubmit={handleSubmit}
-                    method="POST"
-                    action={""} //safari auto fill umgehen
-                    className="mb-10"
-                  >
-                    {/* <input
-                      type="hidden"
-                      name="access_key"
-                      value="cad5a6f2-8ca8-4c75-aa28-2c42ee8905ec"
-                    /> */}
-
-                    <div className="flex w-full gap-5 sm:gap-10 mb-4">
-                      <div>
-                        <label className="block font-semibold text-lg mb-2 ">
-                          Vorname:
-                        </label>
-                        <input
-                          type="text"
-                          name="Vorname"
-                          value={firstName}
-                          onChange={(e) => setFirstName(e.target.value)}
-                          placeholder="Ihren Name"
-                          className="p-2 border border-orange-200 rounded-xl w-full"
-                          required
-                        />
-                      </div>
-                      <div>
-                        <label className="block font-semibold text-lg mb-2 ">
-                          Nachname:
-                        </label>
-                        <input
-                          type="text"
-                          name="Nachname"
-                          value={lastName}
-                          onChange={(e) => setLastName(e.target.value)}
-                          placeholder="Ihren Name"
-                          className="p-2 border border-orange-200 rounded-xl w-full"
-                          required
-                        />
-                      </div>
-                    </div>
-
-                    <div className="mb-4">
-                      <label className="block font-semibold text-lg mb-2 ">
-                        Ihre E-Mail:
-                      </label>
-                      <input
-                        type="email"
-                        name="E-Mail"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        placeholder="Inhalt eingeben"
-                        className="p-2 border border-orange-200 rounded-xl w-full"
-                        required
-                      />
-                    </div>
-
-                    <div className="mb-4">
-                      <label className="block font-semibold text-lg mb-2">
-                        Ihre Nachricht:
-                      </label>
-                      <textarea
-                        name="Nachricht"
-                        value={message}
-                        onChange={(e) => setMessage(e.target.value)}
-                        placeholder="Ihre Nachricht - Wann Sie die Skihütte mieten möchten, wie Sie diese verwenden möchten"
-                        className="p-2 border border-orange-200 rounded-xl w-full h-56"
-                        required
-                      />
-                    </div>
-
-                    {/* FormResultUI - Responsive antwortj */}
-                    <div>
-                      {!success && !error ?
-                        <button
-                          type="submit"
-                          className="bg-orange-500 text-white font-semibold py-2 px-4 rounded-xl hover:bg-orange-600"
-                        >
-                          {loading ? "wird gesendet ..." : "Nachricht Senden"}
-                        </button>
-                        :
-                        <p className="text-center mt-5 -mb-10">
-                          {success && (
-                            <div>
-                              <p className="mb-5" style={{ color: "green" }}>Nachricht wurde erfolgreich gesendet!</p>
-                              <Link
-                                href={"/"}
-                                className="bg-orange-500 text-white font-semibold py-2 px-4 rounded-xl hover:bg-orange-600"
-                              >
-                                Zurück
-                              </Link>
+                                        {/* FormResultUI - Responsive antwort */}
+                                        <div>
+                                            {!success && !error ?
+                                                <button
+                                                    type="submit"
+                                                    className="bg-orange-500 text-white font-semibold py-2 px-4 rounded-xl hover:bg-orange-600"
+                                                >
+                                                    {isSubmitting ? "wird gesendet ..." : "Nachricht Senden"}
+                                                </button>
+                                                :
+                                                <div className="text-center mt-5 -mb-10">
+                                                    {success && (
+                                                        <div>
+                                                            <p className="mb-5" style={{ color: "green" }}>Nachricht wurde erfolgreich gesendet!</p>
+                                                            <Link
+                                                                href={"/"}
+                                                                className="bg-orange-500 text-white font-semibold py-2 px-4 rounded-xl hover:bg-orange-600"
+                                                            >
+                                                                Zurück
+                                                            </Link>
+                                                        </div>
+                                                    )}
+                                                    {error && (
+                                                        <div>
+                                                            <p className="mb-5" style={{ color: "red" }}>Senden Fehlgeschlagen :(</p>
+                                                            <button
+                                                                onClick={() => {
+                                                                    location.reload();
+                                                                }}
+                                                                className="bg-orange-500 text-white font-semibold py-2 px-4 rounded-xl hover:bg-orange-600"
+                                                            >
+                                                                erneut versuchen
+                                                            </button>
+                                                        </div>
+                                                    )}
+                                                </div>
+                                            }
+                                        </div>
+                                    </form>
+                                </CardContent>
                             </div>
-                          )}
-                          {error && (
-                            <div>
-                              <p className="mb-5" style={{ color: "red" }}>Senden Fehlgeschlagen :(</p>
-                              <Link
-                                href={"/"}
-                                onClick={() => {
-                                  // setLoading(false);
-                                  // setError(false);
-                                }}
-                                className="bg-orange-500 text-white font-semibold py-2 px-4 rounded-xl hover:bg-orange-600"
-                              >
-                                erneut versuchen
-                              </Link>
-                            </div>
-                          )}
-                        </p>
-                      }
+                        </Card>
                     </div>
-                  </form>
-                </CardContent>
-              </div>
-            </Card>
-          </div>
+                </div>
+            </section>
         </div>
-      </section>
-    </div>
-  );
+    );
 };
 
 
