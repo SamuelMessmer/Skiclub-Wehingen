6eaa731f715c27ad5e20275349bf240e2fe753fd
Samuel Messmer <samuel.messmer@web.de>
2025-06-24 13:50:34 +0200

    chore: hide overflow x

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
 
       </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-x-hidden`}
       >
         <NavBar />
         {children}
